5e006a57e546ecd726494e3be38e5b1a256ea5fa
Alejandro <david1804997@gmail.com>
2018-09-17 16:30:34 -0500

    Alejandro (#4)
    * Creación de features
    
    * Se añade features (Solo el esquema)

---
+++ src/components/FeatureComponent/Feature.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+    <div class="feature-title">{{name}}</div>
+    <div class="feature-container">
+    <FeatureDescription :name= "name" class="feature-subcontent">
+        <slot name="description"></slot>
+    </FeatureDescription>
+    <FeatureCode class="feature-subcontent">
+        <slot name="code"></slot>
+    </FeatureCode>
+    <FeatureExample class="feature-subcontent">
+        <slot name="example"></slot>
+    </FeatureExample>
+</div> 
+</div>
+</template>
+
+<script>
+import FeatureCode from "./FeatureCode.vue";
+import FeatureDescription from "./FeatureDescription.vue";
+import FeatureExample from "./FeatureExample.vue";
+
+export default {
+  name: "Feature",
+  components: {
+    FeatureCode,
+    FeatureDescription,
+    FeatureExample
+  },
+  props: {
+    name
+  }
+};
+</script>
+
+<style scoped>
+.feature-container {
+  display: flex;
+  flex-flow: nowrap row;
+}
+.feature-subcontent {
+  padding: 20px;
+  text-justify: auto;
+}
+.feature-title{
+      font-size: 1.2em;
+    height: 1.3em;
+}
+</style>
---
+++ src/components/FeatureComponent/FeatureCode.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <pre>
+                    <code>
+                        <slot>
+                        </slot>
+                    </code>
+                    </pre>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "FeatureCode",
+  props: {
+    name
+  }
+};
+</script>
+
+<style scoped>
+pre {
+  color: white;
+  background: black;
+  margin: 0px;
+  border-radius: 10px;
+}
+
+code {
+  margin: 0px;
+}
+</style>
